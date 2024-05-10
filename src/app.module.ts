353e401a6b803a05425d96837b1a49a498bc71da
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-10 23:59:35 +0300

    feat: set up `GraphQL`
    Add graphql module (`GraphQLModule`) to imports array of
    `AppModule` and configure it (`GraphQL`) to be the API of the
    application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
