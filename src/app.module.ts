b2892293a9fc223cc9d9a9d3a4710c28c68bf101
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-10 23:58:24 +0300

    feat: add `mongoose` ORM for dealing with the database
    Install `@nestjs/mongoose` and `mongoose` packages.
    
    Add `MongooseModule` module from `@nestjs/mongoose` package to
    imports array of `AppModule` to include `mongoose` ORM to the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
   providers: [AppService],
 })
 export class AppModule {}
