0be59281c5abfe5a423d1af029a45b1898a53117
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-12 16:57:15 +0300

    feat: create posts module, resolver and service
    Create the main files of `posts` resource: posts module (`posts.module.ts`),
    posts resolver (`posts.resolver.ts`) and posts service (`posts.service.ts`).

---
+++ src/posts/posts.resolver.ts
@@ -0,0 +1,56 @@
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
+import { PostsService } from './posts.service';
+import { Post } from '../global-dto/post-data.type';
+import { CreatePostInput } from './dto/create-post.input';
+import { UpdatePostInput } from './dto/update-post.input';
+import { SearchForPostInput } from './dto/search-for-post.input';
+import { User } from 'src/global-dto/user-data.type';
+import { UsersService } from 'src/users/users.service';
+import { PaginationOptions } from 'src/global-dto/pagination-options.dto';
+import { Types } from 'mongoose';
+import { MongoObjectIdScalar } from 'src/global-dto/mongoObjectId.scalar';
+
+@Resolver(() => Post)
+export class PostsResolver {
+  constructor(
+    private postsService: PostsService,
+    private usersService: UsersService,
+  ) {}
+
+  @Mutation(() => Boolean)
+  async createPost(@Args('createPostInput') createPostInput: CreatePostInput) {
+    return await this.postsService.createPost(createPostInput);
+  }
+
+  @Query(() => [Post], { name: 'posts' })
+  async findPosts(
+    @Args('searchQuery') searchQuery: SearchForPostInput,
+    @Args('options', { nullable: true }) options: PaginationOptions,
+  ) {
+    return await this.postsService.findPosts(searchQuery, options);
+  }
+
+  @Query(() => Post, { name: 'post', nullable: true })
+  async findPostById(
+    @Args('postId', { type: () => MongoObjectIdScalar }) postId: Types.ObjectId,
+  ) {
+    return await this.postsService.findPostById(postId);
+  }
+
+  @ResolveField(() => User, { name: 'author', nullable: true })
+  async findPostAuthor(@Parent() post: Post) {
+    return await this.usersService.findUserById(post.authorId);
+  }
+
+  @Mutation(() => Boolean)
+  async updatePost(@Args('updatePostInput') updatePostInput: UpdatePostInput) {
+    return await this.postsService.updatePost(updatePostInput);
+  }
+}
