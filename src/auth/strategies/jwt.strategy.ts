fc6cc65a9180322a4813e82654f207bd4d5cfe35
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-22 15:32:45 +0300

    feat: create the `JWT` strategy for authorization

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JWTTokenPayload } from '../dto/jwt-token-payload.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: JWTTokenPayload) {
+    return {
+      id: payload.id,
+      email: payload.email,
+      role: payload.role,
+    };
+  }
+}
