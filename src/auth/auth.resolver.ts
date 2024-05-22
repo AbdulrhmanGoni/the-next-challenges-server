6d75212e434074a0bfd8cd6ae740759f160019d4
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-22 15:08:57 +0300

    feat: create `AuthResolver` to handle "log in" & "Sign up" requests.
    Create the auth resolver (`AuthResolver` class) and make it handles
    "log in" and "Sign up" requests.

---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,20 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LogInUserInput } from './dto/log-in-user.input';
+import { SignUpUserInput } from './dto/sign-up-user.input';
+import { JWTToken } from './dto/jwt-token-payload.dto';
+
+@Resolver()
+export class AuthResolver {
+  constructor(private authService: AuthService) { }
+
+  @Mutation(() => JWTToken, { nullable: true })
+  async logInUser(@Args('logInUserInput') logInUserInput: LogInUserInput) {
+    return this.authService.logInUser(logInUserInput);
+  }
+
+  @Mutation(() => JWTToken, { nullable: true })
+  async signUpUser(@Args('signUpUserInput') signUpUserInput: SignUpUserInput) {
+    return this.authService.signUpUser(signUpUserInput);
+  }
+}
