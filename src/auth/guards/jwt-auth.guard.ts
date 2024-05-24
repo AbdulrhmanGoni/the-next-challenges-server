e55e6a3499b4cadb02e753bd9325aec21fb81415
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-24 13:54:43 +0300

    feat: create `JWT` auth guard for routes protection

---
+++ src/auth/guards/jwt-auth.guard.ts
@@ -0,0 +1,19 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { AuthGuard } from '@nestjs/passport';
+import { createParamDecorator } from '@nestjs/common';
+
+@Injectable()
+export class GqlJwtAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req;
+  }
+}
+
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, context: ExecutionContext) => {
+    const ctx = GqlExecutionContext.create(context);
+    return ctx.getContext().req.user;
+  },
+);
