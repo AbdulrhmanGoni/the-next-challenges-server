1deadf9add2654fdeab11505706acbd9e00ed5fe
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-26 10:57:16 +0300

    feat: add `options` parameter to `addPost` method of posts service

--- src/users/services/addPost.ts
+++ src/users/services/addPost.ts
@@ -1,15 +1,17 @@
-import { Model, Types } from 'mongoose';
+import { ClientSession, Model, Types } from 'mongoose';
 import { User } from '../schemas/user.schema';
 
 export default async function addPost(
   userId: Types.ObjectId,
   postId: Types.ObjectId,
+  options?: { session?: ClientSession },
 ) {
   const UserModel = this.UserModel as Model<User>;
 
   const { acknowledged, modifiedCount } = await UserModel.updateOne(
     { _id: userId },
     { $push: { posts: postId } },
+    options,
   );
 
   return acknowledged && !!modifiedCount;
