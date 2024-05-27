673ac4a21013674cb3598284c895d5e9ad32f167
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-27 12:13:53 +0300

    feat: write the implementation of `createPost` method
    Write the implementation of `createPost` method of `PostsService` class

--- src/posts/services/createPost.ts
+++ src/posts/services/createPost.ts
@@ -1,7 +1,40 @@
-import { Model } from 'mongoose';
+import { ClientSession, Connection, Model, Types } from 'mongoose';
 import { CreatePostInput } from '../dto/create-post.input';
 import { Post } from '../schemas/post.schema';
+import { InternalServerErrorException } from '@nestjs/common';
+import { UsersService } from '../../users/users.service';
 
-export default async function createPost(post: CreatePostInput) {
+export default async function createPost(
+  post: CreatePostInput,
+  userId: Types.ObjectId,
+) {
   const PostModel = this.PostModel as Model<Post>;
+  const usersService = this.UsersService as UsersService;
+  const mongodbConnection = this.connection as Connection;
+
+  let session: ClientSession;
+  try {
+    session = await mongodbConnection.startSession();
+    session.startTransaction();
+  } catch {
+    throw new InternalServerErrorException();
+  }
+
+  try {
+    const newPost = new PostModel({ ...post, authorId: userId });
+    const newPostId = (await newPost.save({ session }))._id;
+
+    const postAdded = await usersService.addPost(userId, newPostId, {
+      session,
+    });
+
+    if (postAdded) {
+      await session.commitTransaction();
+    }
+
+    return postAdded;
+  } catch {
+    await session.abortTransaction();
+    throw new InternalServerErrorException();
+  }
 }
