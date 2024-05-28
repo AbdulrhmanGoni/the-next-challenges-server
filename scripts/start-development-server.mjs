d7bc857ce376ee1a0baedf31dcc0291d7779205f
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-28 11:29:25 +0300

    chore: remove `--build` flag from "running dev server" command
    Remove `--build` flag from `docker-compose` command that starts the
    the containers of development server in `start-development-server.mjs`
    file.

--- scripts/start-development-server.mjs
+++ scripts/start-development-server.mjs
@@ -5,6 +5,6 @@ execute('docker-compose -f docker-compose-dev-mongodb.yaml up -d');
 
 initializeMongodbReplSet()
     .then(() => {
-        execute('docker-compose -f docker-compose-dev-server.yaml up -d --build');
+        execute('docker-compose -f docker-compose-dev-server.yaml up -d');
         process.exit();
     })
