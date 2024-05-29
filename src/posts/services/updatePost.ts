9976848262a81e3f74e9044ba7b938d65965d6b3
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-05-29 11:32:03 +0300

    feat: write the implementation of `updatePost` method
    Write the implementation of `updatePost` method of `UsersService` class.

--- src/posts/services/updatePost.ts
+++ src/posts/services/updatePost.ts
@@ -1,7 +1,22 @@
-import { Model } from 'mongoose';
-import { UpdatePostInput } from '../dto/update-post.input';
+import { Model, Types } from 'mongoose';
+import { UpdatePostOptions } from '../dto/update-post.input';
 import { Post } from '../schemas/post.schema';
+import { InternalServerErrorException } from '@nestjs/common';
 
-export default async function updatePost(updatePostInput: UpdatePostInput) {
-  const PostModel = this.PostModel as Model<Post>;
+export default async function updatePost(
+  postId: Types.ObjectId,
+  authorId: Types.ObjectId,
+  updateOptions: UpdatePostOptions,
+) {
+  try {
+    const PostModel = this.PostModel as Model<Post>;
+    const { acknowledged, modifiedCount } = await PostModel.updateOne(
+      { _id: postId, authorId },
+      { $set: updateOptions },
+    );
+
+    return acknowledged && !!modifiedCount;
+  } catch {
+    throw new InternalServerErrorException();
+  }
 }
