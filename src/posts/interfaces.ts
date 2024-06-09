8b1f06ef8b55562720756a68e8738277d49371b0
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-06-09 12:58:34 +0300

    feat: create `PostResourceInterface` in `posts/interfaces.ts` file

--- src/posts/interfaces.ts
+++ src/posts/interfaces.ts
@@ -5,3 +5,15 @@ export abstract class PostThumbnailInterface {
   @Field()
   src: string;
 }
+
+@InterfaceType()
+export abstract class PostResourceInterface {
+  @Field()
+  link: string;
+
+  @Field()
+  title: string;
+
+  @Field()
+  type: string;
+}
