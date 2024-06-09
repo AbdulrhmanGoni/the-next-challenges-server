718f9aa8e9f1938fec52f504a524179184cd7bf6
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-06-09 11:25:03 +0300

    feat: use transaction session to delete a post from 2 collections
    Use `createTransactionSession` function in `deletePost.ts` file to create
    transaction session to handle deleting the targeted post from posts
    collection and user's document in users collection.

--- src/posts/services/deletePost.ts
+++ src/posts/services/deletePost.ts
@@ -1,20 +1,36 @@
 import { Model, Types } from 'mongoose';
 import { Post } from '../schemas/post.schema';
 import { InternalServerErrorException } from '@nestjs/common';
+import createTransactionSession from '../../global/utils/createTransactionSession';
+import { UsersService } from '../../users/users.service';
 
 export default async function deletePost(
   postId: Types.ObjectId,
   authorId: Types.ObjectId,
 ) {
+  const session = await createTransactionSession.bind(this)();
   try {
     const PostModel = this.PostModel as Model<Post>;
-    const { acknowledged, deletedCount } = await PostModel.deleteOne({
-      _id: postId,
-      authorId,
-    });
+    const usersService = this.UsersService as UsersService;
+    const { acknowledged, deletedCount } = await PostModel.deleteOne(
+      { _id: postId, authorId },
+      { session },
+    );
 
-    return acknowledged && !!deletedCount;
+    if (acknowledged && !!deletedCount) {
+      const removingResult = await usersService.removePost(authorId, postId, {
+        session,
+      });
+      if (removingResult) {
+        await session.commitTransaction();
+        return true;
+      }
+    } else {
+      await session.abortTransaction();
+      return false;
+    }
   } catch {
+    await session.abortTransaction();
     throw new InternalServerErrorException();
   }
 }
