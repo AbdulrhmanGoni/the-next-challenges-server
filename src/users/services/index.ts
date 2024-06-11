ff0b0737d418d2c5f5cfd75a922ef90b087d62ab
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-06-11 14:34:03 +0300

    feat: add `getUserBookmarks` function to `UsersService` class as method

--- src/users/services/index.ts
+++ src/users/services/index.ts
@@ -5,3 +5,4 @@ export { default as addPost } from './addPost';
 export { default as removePost } from './removePost';
 export { default as addPostToBookmark } from './addPostToBookmark';
 export { default as removePostFromBookmark } from './removePostFromBookmark';
+export { default as getUserBookmarks } from './getUserBookmarks';
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,6 +7,7 @@ import {
   removePost,
   addPostToBookmark,
   removePostFromBookmark,
+  getUserBookmarks,
 } from './services';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -29,4 +30,6 @@ export class UsersService {
   addPostToBookmark = addPostToBookmark;
 
   removePostFromBookmark = removePostFromBookmark;
+
+  getUserBookmarks = getUserBookmarks;
 }
