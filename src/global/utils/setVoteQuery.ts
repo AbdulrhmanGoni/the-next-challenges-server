0eb3059a380ce3b0954452684e5a69a5d412d644
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-06-18 14:45:16 +0300

    feat: add `rootPath` parameter to `setVoteQuery` function
    Add `rootPath` parameter to `setVoteQuery` function to provide
    an option to create update query for inner fields.

--- src/global/utils/setVoteQuery.ts
+++ src/global/utils/setVoteQuery.ts
@@ -3,53 +3,55 @@ import { Types } from 'mongoose';
 export default function setVoteQuery(
   voteType: 'upvote' | 'downvote',
   userId: Types.ObjectId,
+  rootPath?: string,
 ) {
   const voteField = voteType + 's';
   const undoField = voteType === 'upvote' ? 'downvotes' : 'upvotes';
+  const path = rootPath ? rootPath + '.' : '$';
 
   return {
     $set: {
       [voteField]: {
         $cond: {
-          if: { $in: [userId, `$${voteField}.voters`] },
+          if: { $in: [userId, `${path}${voteField}.voters`] },
           then: {
             voters: {
               $filter: {
-                input: `$${voteField}.voters`,
+                input: `${path}${voteField}.voters`,
                 as: 'voterId',
                 cond: { $ne: [userId, '$$voterId'] },
               },
             },
             totalVotes: {
-              $sum: [`$${voteField}.totalVotes`, -1],
+              $sum: [`${path}${voteField}.totalVotes`, -1],
             },
           },
           else: {
             voters: {
-              $concatArrays: [`$${voteField}.voters`, [userId]],
+              $concatArrays: [`${path}${voteField}.voters`, [userId]],
             },
             totalVotes: {
-              $sum: [`$${voteField}.totalVotes`, 1],
+              $sum: [`${path}${voteField}.totalVotes`, 1],
             },
           },
         },
       },
       [undoField]: {
         $cond: {
-          if: { $in: [userId, `$${undoField}.voters`] },
+          if: { $in: [userId, `${path}${undoField}.voters`] },
           then: {
             voters: {
               $filter: {
-                input: `$${undoField}.voters`,
+                input: `${path}${undoField}.voters`,
                 as: 'voterId',
                 cond: { $ne: [userId, '$$voterId'] },
               },
             },
             totalVotes: {
-              $sum: [`$${undoField}.totalVotes`, -1],
+              $sum: [`${path}${undoField}.totalVotes`, -1],
             },
           },
-          else: `$${undoField}`,
+          else: `${path}${undoField}`,
         },
       },
     },
