9d3477a9e6ebbcd8b4fd9823427a299b1589cd6b
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-06-23 11:44:00 +0300

    feat: add three methods to `PostsCommentsResolver` class to expose them
    Add `addCommentToPost` and `removeCommentFromPost` mutation handlers
    methods and `getPostComments` query handler method to the class
    `PostsCommentsResolver` that exposes these methods to call through
    the GraphQL API.

--- src/post-comments/posts-comments.resolver.ts
+++ src/post-comments/posts-comments.resolver.ts
@@ -1,11 +1,54 @@
-import { Resolver } from '@nestjs/graphql';
+import { CurrentUser, GqlJwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
+import { Types } from 'mongoose';
 import { UseGuards } from '@nestjs/common';
-import { GqlJwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { MongoObjectIdScalar } from '../global/dto/mongoObjectId.scalar';
+import { AuthorizedUser } from '../auth/dto/auth-related.dto';
 import { PostsCommentsService } from './posts-comments.service';
 import { PostsComments } from './schemas/posts-comments.schema';
+import { PostComment } from './dto/comment.type';
+import { PaginationOptions } from '../global/dto/pagination-options.dto';
 
 @Resolver(() => PostsComments)
 @UseGuards(GqlJwtAuthGuard)
 export class PostsCommentsResolver {
   constructor(private PostsCommentsService: PostsCommentsService) {}
+
+  @Mutation(() => Boolean)
+  async addCommentToPost(
+    @Args('postId', { type: () => MongoObjectIdScalar }) postId: Types.ObjectId,
+    @Args('comment') comment: string,
+    @CurrentUser() user: AuthorizedUser,
+  ) {
+    return await this.PostsCommentsService.addCommentToPost(
+      postId,
+      comment,
+      user.id,
+    );
+  }
+
+  @Mutation(() => Boolean)
+  async removeCommentFromPost(
+    @Args('postId', { type: () => MongoObjectIdScalar }) postId: Types.ObjectId,
+    @Args('commentId', { type: () => MongoObjectIdScalar })
+    commentId: Types.ObjectId,
+    @CurrentUser() user: AuthorizedUser,
+  ) {
+    return await this.PostsCommentsService.removeCommentFromPost(
+      postId,
+      commentId,
+      user.id,
+    );
+  }
+
+  @Query(() => [PostComment])
+  async getPostComments(
+    @Args('postId', { type: () => MongoObjectIdScalar }) postId: Types.ObjectId,
+    @Args('paginationOptions') paginationOptions: PaginationOptions,
+  ) {
+    return await this.PostsCommentsService.getPostComments(
+      postId,
+      paginationOptions,
+    );
+  }
 }
