5a3602dfe1488a2cae33c9734de86b1c3c760a55
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-04 11:51:00 +0300

    feat: use `incrementPostCommentsCount` method inside `addCommentToPost` method
    Use use `incrementPostCommentsCount` method of `PostsService` service to
    increment the comments count of the post after adding a new comment.
    
    Use `createTransactionSession` to create a session to update the two collections
    (`postcomments` and `posts` ) together.

--- src/post-comments/services/addCommentToPost.ts
+++ src/post-comments/services/addCommentToPost.ts
@@ -1,6 +1,8 @@
 import { Model, Types } from 'mongoose';
 import { PostsComments, PostComment } from '../schemas/posts-comments.schema';
 import { InternalServerErrorException } from '@nestjs/common';
+import createTransactionSession from '../../global/utils/createTransactionSession';
+import { PostsService } from '../../posts/posts.service';
 
 export default async function addCommentToPost(
   postId: Types.ObjectId,
@@ -9,6 +11,9 @@ export default async function addCommentToPost(
 ) {
   try {
     const PostCommentsModel = this.PostCommentsModel as Model<PostsComments>;
+    const PostsService = this.PostsService as PostsService;
+
+    const session = await createTransactionSession();
 
     const newComment = new PostComment();
     newComment.commenterId = userId;
@@ -19,16 +24,28 @@ export default async function addCommentToPost(
       await PostCommentsModel.updateOne(
         { _id: postId },
         { $push: { comments: newComment } },
+        { session },
       );
 
     if (!matchedCount) {
-      return await PostCommentsModel.create({
-        comments: [newComment],
-        _id: postId,
-      }).then(() => true);
+      return await PostCommentsModel.create(
+        {
+          comments: [newComment],
+          _id: postId,
+        },
+        { session },
+      ).then(() => true);
     }
 
-    return acknowledged && !!modifiedCount;
+    if (
+      acknowledged &&
+      !!modifiedCount &&
+      (await PostsService.incrementPostCommentsCount(postId, session))
+    ) {
+      return await session.commitTransaction().then(() => true);
+    } else {
+      await session.abortTransaction();
+    }
   } catch {
     throw new InternalServerErrorException();
   }
