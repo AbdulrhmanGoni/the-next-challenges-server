cc55e947f7a950b269b578fbd3bce9f46a0d958b
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-07-24 14:48:03 +0300

    feat: create `ImageInterface` & `UserAvatarInterface` interfaces types
    Create `src/interfaces.ts` file and create `ImageInterface` interface type
    inside it.
    
    Create `UserAvatarInterface` interface type in `src/users/interfaces.ts`
    file for all user avatar related types; (`UserAvatarInterface` interface
    type inherits `ImageInterface` interface).
    
    Make `PostThumbnailInterface` interface type inherits `ImageInterface`
    interface.

--- src/posts/interfaces.ts
+++ src/posts/interfaces.ts
@@ -1,7 +1,8 @@
 import { Field, InterfaceType } from '@nestjs/graphql';
+import { ImageInterface } from '../interfaces';
 
 @InterfaceType()
-export abstract class PostThumbnailInterface {
+export abstract class PostThumbnailInterface extends ImageInterface {
   @Field()
   id: string;
 
