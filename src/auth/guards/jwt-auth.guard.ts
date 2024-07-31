4598a27fb85bca1488621dca6c7665646b4738ca
Abdulrahman Goni <abdulrhmangoni@gmail.com>
2024-08-01 01:29:12 +0300

    feat: add additional logic to `GqlJwtAuthGuard` auth guard class
    Add additional logic to `GqlJwtAuthGuard` auth guard class's
    `handleRequest` and `canActivate` methods
    
    - Prevent `handleRequest` method from throwing an error if the
    request is unauthenticated and make it just returns `null` for
    delegating handling authorization process to the next method
    in request handling queue (which is `canActivate`).
    
    - Make `canActivate` method checks if the resource is decorated
    as public or not before applying the actual user role check process.

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -2,12 +2,41 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 import { createParamDecorator } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/is-public.decorator';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class GqlJwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
+  }
+
+  handleRequest<TUser>(
+    ...parapeters: [any, any, any, ExecutionContext, any]
+  ): TUser {
+    try {
+      return super.handleRequest<TUser>(...parapeters);
+    } catch {
+      return null as TUser;
+    }
+  }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    const result = super.canActivate(context);
+    return isPublic || result;
   }
 }
 
