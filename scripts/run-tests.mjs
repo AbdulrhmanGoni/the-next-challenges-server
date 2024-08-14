17ea7c4ff0cddc31bc905b0e2b3e25c31c089bdb
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-14 14:45:03 +0300

    feat: pass testing ENV variables to running test commands
    Use `getTestingENV` function to get testing environment variables
    and pass them to `execute` function (the function that executes or
    runs test commands) as the second parameter.

--- scripts/run-tests.mjs
+++ scripts/run-tests.mjs
@@ -1,18 +1,21 @@
 import { argv } from 'process';
 import execute from './execute.mjs';
 import initializeMongodbReplSet from './initializeMongodbReplSet.mjs';
+import getTestingENV from './getTestingENV.mjs';
 
 const target = argv[2] || "all";
 
+const envVariables = getTestingENV();
+
 if (target === "units") {
-    execute('yarn test:units');
+    execute('yarn test:units', envVariables);
     process.exit();
 } else {
     execute('docker-compose -f docker-compose-testing-mongodb.yaml up -d');
 
     initializeMongodbReplSet()
         .then(() => {
-            execute(`yarn test:${target}`);
+            execute(`yarn test:${target}`, envVariables);
             process.exit();
         })
 }
