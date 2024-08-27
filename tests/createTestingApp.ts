03361270c137c2005f6eb85c09c0283680c202e7
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-08-27 15:15:10 +0300

    feat: add modules & an option to `createTestingApp` fn
    Add `PostsCommentsModule`, `TestingDatabaseModule` and
    `AuthModule` modules to the testing app in `createTestingApp.ts`
    file.
    
    Set `buildSchemaOptions.dateScalarMode` option of `GraphQLModule`
    module to 'timestamp' to the testing app be like the main app
    in `src/app.module.ts`
    
    Remove `autoSchemaFile` option from `GraphQLModule` module.

--- tests/createTestingApp.ts
+++ tests/createTestingApp.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -7,6 +6,9 @@ import { MongoObjectIdScalar } from '../src/global/dto/mongoObjectId.scalar';
 import { UsersModule } from '../src/users/users.module';
 import { PostsModule } from '../src/posts/posts.module';
 import testingDatabaseURI from './testingDatabaseURI';
+import { AuthModule } from '../src/auth/auth.module';
+import { PostsCommentsModule } from '../src/post-comments/posts-comments.module';
+import { TestingDatabaseModule } from './TestingDatabaseModule';
 
 export default async function createTestingApp() {
   const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,11 +16,16 @@ export default async function createTestingApp() {
       MongooseModule.forRoot(testingDatabaseURI),
       GraphQLModule.forRoot<ApolloDriverConfig>({
         driver: ApolloDriver,
-        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
         resolvers: { MongoObjectId: MongoObjectIdScalar },
+        buildSchemaOptions: {
+          dateScalarMode: 'timestamp',
+        },
       }),
       UsersModule,
       PostsModule,
+      AuthModule,
+      PostsCommentsModule,
+      TestingDatabaseModule,
     ],
   }).compile();
 
